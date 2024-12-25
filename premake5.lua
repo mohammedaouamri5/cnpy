613a883cffd3e8db2f6213643dec75c26f4a9e81
mohammedaouamri5 <mohammedaouamri5@gmail.com>
2024-12-25 12:33:53 +0100

    add build system

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+project "cnpy"
+	kind "StaticLib"
+	language "C"
+	staticruntime "off"
+	warnings "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files {
+	"cnpy.cpp" , "cnpy.h" ,
+	}
+	links {
+		"z"
+	}
+
+	filter "system:linux"
+		pic "On"
+
+		systemversion "latest"
+		
+
+
+	filter "system:windows"
+		systemversion "latest"
+
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "speed"
+
+    filter "configurations:Dist"
+		runtime "Release"
+		optimize "speed"
+        symbols "off"
